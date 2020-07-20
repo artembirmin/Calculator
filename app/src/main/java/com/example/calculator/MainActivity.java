0f92faf842e6418c66b680dee8f0d09ed4a8d06c
Artem <disartbs@ya.ru>
2020-07-20 09:30:34 +0300

    Начата работа над полем ввода вывода. Поле обладает курсором нужной высоты и цвета. Подчеркивание поля сделано прозрачным. Клавиатура не выскакивает при нажатии

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.example.calculatop.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
+                WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM); //Скрыло клавиатуру
     }
 }
