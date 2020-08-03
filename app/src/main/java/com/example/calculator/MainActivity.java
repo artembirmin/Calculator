13e9ac634eeab5b8440137faf9bff7203a21264a
Unknown <disartbs@ya.ru>
2020-08-03 14:41:18 +0300

    Рабочий backspace. Умножение и плюс по центру. Динамическое изменение размера текста.. Идет работа над ,

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,65 +1,73 @@
 package com.example.calculator;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.calculatop.R;
-
-import java.text.NumberFormat;
-import java.util.Locale;
+import static com.example.calculator.R.id.btn_division;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText inputField;
     TextView outputField;
     Editable inputForField;
-    StringBuilder inputForCalculations;
-    int rightSelectionPositions = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
                 WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM); //Скрыло клавиатуру
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         inputField = (EditText) findViewById(R.id.edittext_input);
         outputField = (TextView) findViewById(R.id.textview_output);
-        inputField.setText("1 233 123");
+        Button backspace = (Button) findViewById(R.id.btn_backspace);
+        backspace.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                inputField.setText("99+");
+                StringUtil.checkTextSize(inputField);
+                return true;
+            }
+        });
     }
 
     public void onNumberClick(View view){
-
-       // int numberOfSpaces;
         Button button = (Button) view;
         inputForField = inputField.getText();
-
-        int selectionStart = inputField.getSelectionStart();
-        inputForField.insert(selectionStart,button.getText());
-        rightSelectionPositions = inputForField.length() - selectionStart - 1;
-
-
-
-
-        inputField.setText(StringUtil.separation(inputForField.toString(), selectionStart+1));//+1, т.к минммум на 1 позицию будет сдвиг
-        inputField.setSelection(inputField.length() - rightSelectionPositions);
+        inputForField.insert(inputField.getSelectionStart(),button.getText());
+        StringUtil.separation(inputField);
+        StringUtil.checkTextSize(inputField);
     }
 
     public void onOperationClick(View view) {
         Button button = (Button) view;
-       //if(button.getText() == "C"){
-            inputField.setText("");
-      //  }
-
-
-
+        switch (button.getId()){
+            case R.id.btn_clear:
+                inputField.setText("");
+                StringUtil.checkTextSize(inputField);
+                break;
+            case R.id.btn_backspace:
+                Operations.backspace(inputField);
+                StringUtil.checkTextSize(inputField);
+                break;
+            case R.id.btn_sum:
+            case R.id.btn_difference:
+            case R.id.btn_multiplication:
+            case btn_division:
+                StringUtil.append((String) button.getText(), inputField);
+                StringUtil.checkTextSize(inputField);
+                break;
+            case R.id.bnt_fraction:
+                Operations.fraction(inputField);
+                StringUtil.checkTextSize(inputField);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/calculator/StringUtil.java
+++ app/src/main/java/com/example/calculator/StringUtil.java
@@ -1,30 +1,34 @@
 package com.example.calculator;
 
-import android.util.Log;
+import android.widget.EditText;
 
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class StringUtil {
-    public static String separation(String inputStr, int selection) {
-       // if(inputStr.length() == 1)
-        //    return inputStr;
+public abstract class StringUtil {
+
+    private static  ArrayList<Character> numeral= new ArrayList<Character>(Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+    private static final String TAG = "qwerty" ;
+
+    public static void separation(EditText inputField) {
+        if(inputField.getText().length() == 0) return;
         int startNumber;
         int endNumber;
-        int i;
-        StringBuilder input = new StringBuilder(inputStr);
+        int selection = inputField.getSelectionStart() ;
+        int rightSelectionPositions = inputField.getText().length() - selection;
+        StringBuilder input = new StringBuilder(inputField.getText());
+        if(selection != 0 && !numeral.contains(input.charAt(selection - 1)) && input.charAt(selection-1) != ' ') return;
         input.insert(0,'x');
         input.insert(input.length(), 'x');
-        Log.i("qwert-2", input.toString());
+        int i;
         for(i = selection; (input.charAt(i) >= '0' && input.charAt(i) <= '9') || input.charAt(i) == ' '; i--) {
         }
+        if(input.charAt(i) == ',') return;
         startNumber = ++i;
-        for(i = selection; (input.charAt(i) >= '0' && input.charAt(i) <= '9') || input.charAt(i) == ' '; i++) {
+        for(i = selection;(input.charAt(i) >= '0' && input.charAt(i) <= '9') || input.charAt(i) == ' '; i++) {
         }
         endNumber = i;
-        Log.i("qwert-1", input.toString());
         StringBuilder number = new StringBuilder(input.substring(startNumber,endNumber).replaceAll("\\s","")).reverse();
-        Log.i("qwert0", number.toString());
         i = 0;
         for(int j = 0; j < number.length(); j++){
             if( (i+1)%3 == 0){
@@ -37,13 +41,66 @@ public class StringUtil {
         if(number.charAt(number.length()-1) == ' ')
             number.deleteCharAt(number.length()-1);
         number.reverse();
-        Log.i("qwert1", number.toString());
         input.deleteCharAt(0);
         input.deleteCharAt(input.length()-1);
         input.replace(startNumber-1,endNumber-1, number.toString());
-
-        Log.i("qwert2", input.toString());
-        return input.toString();
+        inputField.setText(input);
+        inputField.setSelection(inputField.getText().length() - rightSelectionPositions);
     }
 
+
+    public static void append(String operation, EditText inputField){
+        StringBuilder input = new StringBuilder(inputField.getText());
+        int selection = inputField.getSelectionStart();
+        if(selection != input.length() && input.charAt(selection) == ' ') //Если знак ставится после пробела, сводим к случаю постановки перед пробелом. Он решается автоматом
+            selection++;
+        if(input.length() == 0 && operation.equals("−")) {
+            input.insert(selection, operation);
+            inputField.setText(input.toString());
+            inputField.setSelection(selection + 1);
+            return;
+        }
+        else if(selection == 0){
+            input.insert(selection, operation);
+            inputField.setText(input.toString());
+            inputField.setSelection(selection + 1);
+            return;
+        }
+        else if(input.charAt(selection - 1) != '−' && !numeral.contains(input.charAt(selection - 1)) && operation.equals("−")){
+            input.insert(selection, operation);
+            inputField.setText(input.toString());
+            inputField.setSelection(selection + 1);
+            StringUtil.serapationForOperatoin(inputField);
+            return;
+        } else if(input.charAt(selection - 1) == '−')
+            return;
+        input.insert(selection, operation);
+        inputField.setText(input.toString());
+        inputField.setSelection(selection + 1);
+        StringUtil.serapationForOperatoin(inputField);
+    }
+
+    public static void serapationForOperatoin(EditText inputField) {
+        StringBuilder input = new StringBuilder(inputField.getText());
+        int selection = inputField.getSelectionStart();
+        //if(input.charAt(selection - 2) ==  || input.charAt(selection))
+        int rightSelectionPositions = inputField.getText().length() - selection;
+        inputField.setSelection(selection-1);
+        if(input.length() == 1)
+            return;
+        StringUtil.separation(inputField);
+        inputField.setSelection(inputField.getText().length() - rightSelectionPositions);
+    }
+
+    public static void checkTextSize(EditText inputField){
+        if(inputField.length() <=11)
+            inputField.setTextSize(50);
+        else if( inputField.length() < 14 && inputField.length() >= 12)
+            inputField.setTextSize(48);
+        else if(inputField.length() < 17 && inputField.length() >=14)
+            inputField.setTextSize(40);
+        else if(inputField.length() < 19 && inputField.length() >= 17)
+            inputField.setTextSize(34);
+        else inputField.setTextSize(30);
+    }
 }
